465b4aa2fc1e128b63e9588085bc3c846c03481d
Rukshan <ruky68@live.com>
2017-04-12 11:37:14 +0530

    modify the client

--- src/main/java/com/rukspot/sample/am/event/Publisher.java
+++ src/main/java/com/rukspot/sample/am/event/Publisher.java
@@ -1,21 +1,15 @@
 package com.rukspot.sample.am.event;
 
-import java.util.List;
-import java.util.Map;
-
-import org.wso2.carbon.apimgt.usage.publisher.DataPublisherUtil;
 import org.wso2.carbon.apimgt.usage.publisher.dto.DataBridgeRequestPublisherDTO;
 import org.wso2.carbon.apimgt.usage.publisher.dto.RequestPublisherDTO;
 import org.wso2.carbon.databridge.agent.DataPublisher;
-import org.wso2.carbon.databridge.commons.Event;
 import org.wso2.carbon.databridge.commons.exception.MalformedStreamDefinitionException;
 import org.wso2.carbon.databridge.core.exception.DataBridgeException;
 import org.wso2.carbon.databridge.core.exception.StreamDefinitionStoreException;
 
 public class Publisher {
 	static {
-		System.setProperty("javax.net.ssl.trustStore",
-				"/home/rukshan/wso2-jks/wso2carbon.jks");
+		System.setProperty("javax.net.ssl.trustStore","/mnt/172.30.9.75/wum/wso2am-2.0.0/repository/resources/security/wso2carbon.jks");
 		System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
 	}
 
@@ -23,45 +17,16 @@ public class Publisher {
 			InterruptedException, StreamDefinitionStoreException,
 			MalformedStreamDefinitionException {
 
-		System.setProperty("carbon.home",
-				"/home/rukshan/apim/2.0.x/wso2analytics-apim-1.0.0-SNAPSHOT");
-
-		String STREAM_NAME = "org.wso2.esb.MediatorStatistics";
-		String VERSION = "1.0.0";
-
-		String STREAM_DEFN = "{" + "  'name':'" + STREAM_NAME + "',"
-				+ "  'version':'" + VERSION + "',"
-				+ "  'nickName': 'Stock Quote Information',"
-				+ "  'description': 'Some Desc'," + "  'tags':['foo', 'bar'],"
-				+ "  'metaData':["
-				+ "          {'name':'ipAdd','type':'STRING'}" + "  ],"
-				+ "  'payloadData':["
-				+ "          {'name':'symbol','type':'STRING'},"
-				+ "          {'name':'price','type':'DOUBLE'},"
-				+ "          {'name':'volume','type':'INT'},"
-				+ "          {'name':'max','type':'DOUBLE'},"
-				+ "          {'name':'min','type':'Double'}" + "  ]" + "}";
-
-		String testStream = "{\n" + "  \"name\": \"test\",\n"
-				+ "  \"version\": \"1.0.0\",\n" + "  \"nickName\": \"\",\n"
-				+ "  \"description\": \"\",\n" + "  \"payloadData\": [\n"
-				+ "    {\n" + "      \"name\": \"num\",\n"
-				+ "      \"type\": \"INT\"\n" + "    }\n" + "  ]\n" + "} ";
-
-		// ThriftTestServer thriftTestServer = new ThriftTestServer();
-		// thriftTestServer.start(7613);
-		// thriftTestServer.addStreamDefinition(STREAM_DEFN, -1234);
-		// thriftTestServer.addStreamDefinition(testStream, -1234);
-		// thriftTestServer.addStreamDefinition(DataBridgeRequestPublisherDTO.getStreamDefinition(),
-		// -1234);
+		System.setProperty("carbon.home","/mnt/172.30.9.75/wum/wso2am-2.0.0");
 
 		try {
-			DataPublisher p = new DataPublisher("tcp://localhost:7612",
-					"admin", "admin");
-
+//			DataPublisher p = new DataPublisher("tcp://localhost:7612","admin", "admin");
+			String url ="tcp://172.30.9.144:7611";
+			DataPublisher p = new DataPublisher(url, "admin","bA3RxeyU6gvt");
+			
 			int i = 0;
 			int s = 0;
-			for (int j = s; j < s + 5000; j++) {
+			for (int j = s; j < s + 1; j++) {
 				i = j;
 				String consumerKey = "Md7pgErNWgtR6p6Cxlv016kzueI" + i;
 				String username = "admin";
@@ -109,23 +74,15 @@ public class Publisher {
 
 				DataBridgeRequestPublisherDTO dt = new DataBridgeRequestPublisherDTO(
 						requestPublisherDTO);
-//				Object[] ar = (Object[]) dt.createPayload();
-				Object[] ar= new Object[]{"key","nbame","version"};
+				Object[] ar = (Object[]) dt.createPayload();
 				
 				String id = "org.wso2.apimgt.statistics.request:1.1.0";
+				System.out.println("sending...");
 				p.publish(id, new Object[] { "external" }, null, ar);
 				System.out.println("sent "+i);
+				
 			}
-			/*
-			 * while(thriftTestServer.getNumberOfEventsReceived()<1);
-			 * System.out.println(thriftTestServer.getNumberOfEventsReceived());
-			 * 
-			 * Map<String, List<Event>>
-			 * dataTables=thriftTestServer.getDataTables();
-			 * System.out.println("t :"+dataTables.size());
-			 * System.out.println("t :"
-			 * +dataTables.get(id).get(0).getTimeStamp());
-			 */
+
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
